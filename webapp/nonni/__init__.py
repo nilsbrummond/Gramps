bdea19a9aefdc08de326b9da0b7da27a3be3576b
Doug Blank <doug.blank@gmail.com>
2009-08-12 05:43:11 +0000

    some test functions to display data
    git-svn-id: svn+ssh://svn.code.sf.net/p/gramps/code/branches/geps/gep-013-server@12971 4ae1f11a-8b86-4847-b8af-ab372f36d1fd

---
+++ webapp/nonni/__init__.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404, render_to_response
+
+from nonni.nonna.models import Person
+
+def home(request, *args, **kwargs):
+    content = {
+        'title': 'Homepage - ',
+        'heading': 'My Family Tree',
+        'footer':  'This is the footer!',
+        'copyright': 'Copyright (c) 2009, by Me.',
+        'person_list': Person.objects.filter(gender=2),
+        #'meta': ,
+        }
+    add_content(content)
+    return render_to_response('base.html', content)
+                              
+
+def add_content(data):
+    content = {
+        'content': 'People with Unknown Gender:',
+    }
+    return data.update(content)
