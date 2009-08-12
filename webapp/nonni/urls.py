9a1088d5886ab46805f7d5ba9d40891f684a3fb0
Doug Blank <doug.blank@gmail.com>
2009-08-12 05:43:56 +0000

    added static directories
    git-svn-id: svn+ssh://svn.code.sf.net/p/gramps/code/branches/geps/gep-013-server@12972 4ae1f11a-8b86-4847-b8af-ab372f36d1fd

--- webapp/nonni/urls.py
+++ webapp/nonni/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # ORDER MATTERS!
     # Example:
     # (r'^nonni/', include('nonni.foo.urls')),
 
@@ -12,6 +13,21 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    # DANGEROUS in production:
+     (r'^styles/(?P<path>.*)$', 'django.views.static.serve',
+      {'document_root': 
+          '/home/dblank/gramps/gep-013-server/webapp/html/styles', 
+       'show_indexes': 
+          True},
+      ),
+     (r'^images/(?P<path>.*)$', 'django.views.static.serve',
+      {'document_root': 
+          '/home/dblank/gramps/gep-013-server/webapp/html/images', 
+       'show_indexes': 
+          True},
+      ),
     # Uncomment the next line to enable the admin:
      (r'^admin/(.*)', admin.site.root),
+    # Finally, root:
+     (r'^', 'nonni.home'),
 )
