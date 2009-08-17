d77ec3bb132e95e53e6225c0f589a8eb7199a0af
Doug Blank <doug.blank@gmail.com>
2009-08-17 15:20:06 +0000

    code clean up: remove len(map) and use new util functions
    git-svn-id: svn+ssh://svn.code.sf.net/p/gramps/code/branches/geps/gep-013-server@13045 4ae1f11a-8b86-4847-b8af-ab372f36d1fd

--- src/plugins/export/ExportDjango.py
+++ src/plugins/export/ExportDjango.py
@@ -94,38 +94,7 @@ def make_db():
     """
     Prepares the database.
     """
-    dj.Address.objects.all().delete()
-    #dj.AttributeType.objects.all().delete()
-    dj.ChildRef.objects.all().delete()
-    #dj.ChildRefType.objects.all().delete()
-    dj.Event.objects.all().delete()
-    dj.EventRef.objects.all().delete()
-    #dj.EventRoleType.objects.all().delete()
-    #dj.EventType.objects.all().delete()
-    dj.Family.objects.all().delete()
-    #dj.FamilyRelType.objects.all().delete()
-    #dj.GenderType.objects.all().delete()
-    dj.Lds.objects.all().delete()
-    dj.Location.objects.all().delete()
-    #dj.MarkerType.objects.all().delete()
-    dj.Markup.objects.all().delete()
-    dj.Media.objects.all().delete()
-    dj.MediaRef.objects.all().delete()
-    dj.Name.objects.all().delete()
-    #dj.NameType.objects.all().delete()
-    dj.Note.objects.all().delete()
-    dj.NoteRef.objects.all().delete()
-    #dj.NoteType.objects.all().delete()
-    dj.Person.objects.all().delete()
-    dj.PersonRef.objects.all().delete()
-    dj.Place.objects.all().delete()
-    dj.Repository.objects.all().delete()
-    dj.RepositoryRef.objects.all().delete()
-    #dj.RepositoryType.objects.all().delete()
-    dj.Source.objects.all().delete()
-    #dj.SourcemediaType.objects.all().delete()
-    dj.SourceRef.objects.all().delete()
-    #dj.UrlType.objects.all().delete()
+    dj.clear_tables("primary", "secondary")
 
 ## Export lists:
 
@@ -695,14 +664,14 @@ def export_all(database, filename, option_box=None, callback=None):
         callback = lambda (percent): None # dummy
 
     start = time.time()
-    total = (len(database.note_map) + 
-             len(database.person_map) +
-             len(database.event_map) + 
-             len(database.family_map) +
-             len(database.repository_map) +
-             len(database.place_map) +
-             len(database.media_map) +
-             len(database.source_map)) * 2 # steps
+    total = (database.get_number_of_notes() + 
+             database.get_number_of_people() +
+             database.get_number_of_events() + 
+             database.get_number_of_families() +
+             database.get_number_of_repositories() +
+             database.get_number_of_places() +
+             database.get_number_of_media_objects() +
+             database.get_number_of_sources()) * 2 # steps
     count = 0.0
     make_db()
 
