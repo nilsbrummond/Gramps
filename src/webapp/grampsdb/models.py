4de36090e6c5f3a2e8c945ba121bd4e96eb8540a
Doug Blank <doug.blank@gmail.com>
2012-07-27 06:21:50 +0000

    Django Database changes to allow ordered items (families, tags, etc); everything which is import/export is 100% correct
    git-svn-id: svn+ssh://svn.code.sf.net/p/gramps/code/branches/gramps35@20099 4ae1f11a-8b86-4847-b8af-ab372f36d1fd

--- src/webapp/grampsdb/models.py
+++ src/webapp/grampsdb/models.py
@@ -469,6 +469,26 @@ class PersonParentFamilyOrder(models.Model):
     family = models.ForeignKey("Family")
     order = models.PositiveIntegerField(default=1)
 
+class PersonTag(models.Model):
+    person = models.ForeignKey("Person")
+    tag = models.ForeignKey("Tag")
+    order = models.PositiveIntegerField(default=1)
+
+class FamilyTag(models.Model):
+    family = models.ForeignKey("Family")
+    tag = models.ForeignKey("Tag")
+    order = models.PositiveIntegerField(default=1)
+
+class MediaTag(models.Model):
+    media = models.ForeignKey("Media")
+    tag = models.ForeignKey("Tag")
+    order = models.PositiveIntegerField(default=1)
+
+class NoteTag(models.Model):
+    note = models.ForeignKey("Note")
+    tag = models.ForeignKey("Tag")
+    order = models.PositiveIntegerField(default=1)
+
 class Person(PrimaryObject):
     """
     The model for the person object
@@ -490,7 +510,7 @@ class Person(PrimaryObject):
     birth_ref_index = models.IntegerField("Birth Reference Index", default=-1)
     death_ref_index = models.IntegerField("Death Reference Index", default=-1)
 
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True, through="PersonTag")
 
     # Others keys here:
     #   .name_set 
@@ -519,7 +539,7 @@ class Family(PrimaryObject):
     mother = models.ForeignKey('Person', related_name="mother_ref", 
                                null=True, blank=True)
     family_rel_type = models.ForeignKey('FamilyRelType', verbose_name="Type")
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True, through="FamilyTag")
 
     def make_tag_list(self):
         return tuple()
@@ -614,7 +634,7 @@ class Media(DateObject, PrimaryObject):
     references = generic.GenericRelation('MediaRef', related_name="refs",
                                          content_type_field="object_type",
                                          object_id_field="object_id")
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True, through="MediaTag")
 
     def make_tag_list(self):
         return tuple()
@@ -629,7 +649,7 @@ class Note(PrimaryObject):
     references = generic.GenericRelation('NoteRef', related_name="refs",
                                          content_type_field="object_type",
                                          object_id_field="object_id")
-    tags = models.ManyToManyField('Tag', blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True, null=True, through="NoteTag")
 
     def make_tag_list(self):
         return tuple()
@@ -1033,6 +1053,12 @@ TABLES = [
     ("ref", PersonRef),
     ("ref", ChildRef),
     ("ref", MediaRef),
+    ("ref", PersonFamilyOrder),
+    ("ref", PersonParentFamilyOrder),
+    ("ref", PersonTag),
+    ("ref", FamilyTag),
+    ("ref", MediaTag),
+    ("ref", NoteTag),
     ("system", Config),
     ("system", Report),
     ("system", Result),
